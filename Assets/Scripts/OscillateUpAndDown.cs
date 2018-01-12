408562ce21b5999bfd543e748086a8254b3bf1b5
Mario Gutierrez <0x4d4147@gmail.com>
2018-01-12 03:01:04 -0600

    spaghet

---
+++ Assets/Scripts/OscillateUpAndDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillateUpAndDown : MonoBehaviour
+{
+    public float frequency;
+    public float amplitude;
+
+    Vector3 initialPosition;
+
+    void Awake()
+    {
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = initialPosition + Vector3.up * Mathf.Sin(Time.time * frequency) * amplitude;
+    }
+}
